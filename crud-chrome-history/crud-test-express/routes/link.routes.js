de802d9afcbdc8c3479c6343b2f5cd2fcf977afa
vds-tanuj4567 <vdstanuuj4567@gmail.com>
2019-05-18 23:56:55 +1000

    routes created for insertion and retrieval and debugged using postman

--- crud-chrome-history/crud-test-express/routes/link.routes.js
+++ crud-chrome-history/crud-test-express/routes/link.routes.js
@@ -5,6 +5,6 @@ const router=Express.Router()
 console.log(X.insert)
 router.get('/:id', X.getOne);
 router.get('/', X.getAll);
-router.post('/insert/:id', X.insert);
+router.post('/insert/:id', X.insert); //should check using postman 
 
 module.exports={"router":router};
