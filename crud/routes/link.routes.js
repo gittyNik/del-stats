2d3459857c976493d1b3b0a91d99745abadd7d0b
Meghana-Chigarapalle <meghana25293@gmail.com>
2019-05-30 20:50:10 +0530

    Deleted unwanted files

--- crud/routes/link.routes.js
+++
@@ -1,9 +0,0 @@
-var Express = require('express');
-
-var X = require('../controllers/link.controller');
-const router = Express.Router();
-
-router.post('/insert/:uid/:topic/:url', X.insert);
-router.get('/:topic', X.select);
-
-module.exports = { "router": router };
--- crud/src.js
+++
@@ -1,9 +0,0 @@
-var Exp = require('express')
-var app = Exp()
-var bodyParser = require("body-parser");
-var u = require('./routes/link.routes.js')
-console.log(u.router)
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use('/', u.router)
-app.listen(3000);
