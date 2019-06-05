14898ae3cefbd0dc889859a4ae0d05e7c8503d15
Meghana-Chigarapalle <meghana25293@gmail.com>
2019-06-05 10:43:05 +0530

    Updated routes to avoid ambiguity

--- src/routes/link.routes.js
+++ src/routes/link.routes.js
@@ -4,18 +4,18 @@ import {get_resources,get_resource,update_resource,delete_resource,get_comments,
 const router = Express.Router();
 
 router.get('/resources',get_resources);
-router.get('/resources/:resource_id',get_resource);
-router.patch('/resources/:resource_id',update_resource);
-router.delete('/resources/:resource_id',delete_resource);
-router.get('/resources/:resource_id/comments',get_comments);
-router.post('/resources/:resource_id/comments',insert_comment);
-router.delete('/resources/:resource_id/comments/:comment_id',delete_comment);
+router.get('/resources/:resource_id([0-9]*)',get_resource);
+router.patch('/resources/:resource_id([0-9]*)',update_resource);
+router.delete('/resources/:resource_id([0-9]*)',delete_resource);
+router.get('/resources/:resource_id([0-9]*)/comments',get_comments);
+router.post('/resources/:resource_id([0-9]*)/comments',insert_comment);
+router.delete('/resources/:resource_id([0-9]*)/comments/:comment_id([0-9]*)',delete_comment);
 router.post('/resources/:resource_id/vote',upvote);
-router.delete('/resources/:resource_id/vote',downvote);
+router.delete('/resources/:resource_id([0-9]*)/vote',downvote);
 router.get('/resources/reports',get_report);
-router.post('/resources/:resource_id/reports',insert_report);
-router.patch('/resources/:resource_id/reports/:report_id/resolve',update_report);
+router.post('/resources/:resource_id([0-9]*)/reports',insert_report);
+router.patch('/resources/:resource_id([0-9]*)/reports/:report_id([0-9]*)/resolve',update_report);
 router.get('/resources/pending',unmoderated_requests);
-router.patch('/resources/:resource_id/approve',approve_resource);
+router.patch('/resources/:resource_id([0-9]*)/approve',approve_resource);
 
 export default router;
