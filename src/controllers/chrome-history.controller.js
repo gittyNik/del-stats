aef486ff34374f1445be87743a0b98769c1cde5a
vds-tanuj4567 <vdstanuuj4567@gmail.com>
2019-06-11 04:50:17 +1000

    local storage for user id and ip address code added

--- src/controllers/chrome-history.controller.js
+++ src/controllers/chrome-history.controller.js
@@ -1,5 +1,6 @@
 import {links} from '../models/chrome-history';
-
+var localStorage=require('localStorage');
+var ip=require('ip');
 export const getAll= function(req,res){links.findAll({}).then((data) => {
     res.json(data)
   }).catch((err) => {
@@ -20,7 +21,7 @@ export const getMax= (req,res)=>{
   links.findAll({ 
     attributes: [[sequelize.fn('max', sequelize.col('visited_timestamp')), 'time']],
     where:{
-      user_id:'1256'
+      user_id:localStorage.getItem('userId')
     },
     raw: true,
   }).then((data) => {
@@ -28,4 +29,70 @@ export const getMax= (req,res)=>{
   }).catch((err) => {
   console.log(err);
   })
+}
+
+export const insert= function(req,res){  
+  var historyitem=req.body.historyitem;
+  var gid=req.body.getid;
+  console.log("welcome");
+  console.log(localStorage.getItem('userId'));
+  console.log(historyitem);
+  var Resource=require('../models/chrome-history');     
+  ( function(historyitem,gid){
+      var sequelize=require('sequelize');
+       Resource.links.findAll({ 
+      attributes: [[sequelize.fn('max', sequelize.col('visited_timestamp')), 'time']],
+      where:{
+        user_id:gid
+      },
+      raw: true,
+    }).then( async (data) => {
+      for(var i=0;i<historyitem.length;i++){
+         var promise=new Promise((resolve,reject)=>{(function(i,historyitem,gid,data){Resource.links.sync({force: false}).then(function () {
+        if(data[0]['time']==null || data[0]['time']<historyitem[i]['lastVisitTime'])
+       { 
+         console.log("right");
+         console.log(historyitem[i]['lastVisitTime']);
+        return Resource.links.create({
+          user_id: gid,
+          url: historyitem[i].url,
+          ip:  ip.address(),
+          visited_timestamp: historyitem[i]['lastVisitTime'],
+          title: historyitem[i]['title'],
+          visitcount: historyitem[i]['visitCount'],
+          useragent: historyitem[i]['userAgent']
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+      }
+      else{
+        console.log(historyitem[i]['lastVisitTime']);
+        console.log(data[0]['time']);
+        console.log("wrong");
+        return true;
+      }
+      
+      
+      
+  }
+      )
+      .catch(function(err) {
+        console.log(err);
+    })})(i,historyitem,gid,data);
+
+    
+    resolve("hello");
+    });
+    let k =await  promise;
+    console.log("hero");
+    } }
+    
+    ).catch((err) => {
+    console.log(err);
+    })
+    
+    
+    
+  })(historyitem,gid);
 }
--- src/routes/chrome-history.routes.js
+++ src/routes/chrome-history.routes.js
@@ -1,82 +1,8 @@
 var Express=require('express')
 var controller=require('../controllers/chrome-history.controller')
 const router=Express.Router()
-
-router.post('/insert',function(req,res){  
-    var historyitem=req.body.historyitem;
-    var gid=req.body.getid;
-    console.log("welcome");
-    var Resource=require('../models/chrome-history');
-    
-    
-        
-      ( function(historyitem,gid){
-        var sequelize=require('sequelize');
-         Resource.links.findAll({ 
-        attributes: [[sequelize.fn('max', sequelize.col('visited_timestamp')), 'time']],
-        where:{
-          user_id:gid
-        },
-        raw: true,
-      }).then( (data) => {
-        for(var i=0;i<historyitem.length;i++){
-           (function(i,historyitem,gid,data){Resource.links.sync({force: false}).then(function () {
-          if(data[0]['time']==null || data[0]['time']<historyitem[i]['lastVisitTime'])
-         { 
-          return Resource.links.create({
-            user_id: gid,
-            url: historyitem[i].url,
-            ip:  historyitem[i]['ip'],
-            visited_timestamp: historyitem[i]['lastVisitTime'],
-            title: historyitem[i]['title'],
-            visitcount: historyitem[i]['visitCount'],
-            useragent: historyitem[i]['userAgent']
-          })
-          .catch(function(err) {
-              console.log(err);
-          });
-        }
-        else{
-          return true;
-        }
-        })
-        .catch(function(err) {
-          console.log(err);
-      })})(i,historyitem,gid,data);
-
-      console.log("hero")
-
-      } }).catch((err) => {
-      console.log(err);
-      })
-      
-      
-      
-    })(historyitem,gid);
-    
-    
-      
-    /*(function(i,historyitem,gid){Resource.links.sync({force: false}).then(function () {
-      return Resource.links.create({
-        user_id: gid,
-        url: historyitem[i].url,
-        ip:  historyitem[i]['ip'],
-        visited_timestamp: historyitem[i]['lastVisitTime'],
-        title: historyitem[i]['title'],
-        visitcount: historyitem[i]['visitCount'],
-        useragent: historyitem[i]['userAgent']
-      })
-      .catch(function(err) {
-          console.log(err);
-      });
-    })
-    .catch(function(err) {
-      console.log(err);
-  })})(i,historyitem,gid);*/
-
-
-  
-});
+const ip=require('ip')
+router.post('/insert',controller.insert);
 
 router.get('/allresults', controller.getAll);
 router.get('/max',controller.getMax);
