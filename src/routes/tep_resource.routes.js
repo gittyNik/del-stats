e19db86df5e98c40e80b29ba618b05e666b15f3a
Meghana-Chigarapalle <meghana25293@gmail.com>
2019-06-12 15:12:35 +0530

    added documentation for routes performing crud operation on resources

--- src/routes/tep_resource.routes.js
+++ src/routes/tep_resource.routes.js
@@ -2,19 +2,38 @@ import Express from 'express';
 
 import { getLatest, getTop, getAll, getUnmoderated, getOne, create, update,
   deleteOne, getComments, addComment, deleteComment, upvote, unvote, getReports,
-  addReport, resolveReport, approve } from '../controllers/tep_resource';
+  addReport, resolveReport, approve } from '../controllers/tep_resource.controller';
 
 const router = Express.Router();
 
-
+/**
+ * @api {get} /tep/resources/reports Get all TEP resource reports
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName GetAllReports
+ * @apiGroup TepResources
+ */
 router.get('/reports', getReports);
+
+/**
+ * @api {post} /tep/resources/:resource_id/reports Post new TEP resources
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName AddReport
+ * @apiGroup TepResources
+ */
 router.post('/:resource_id/reports', addReport);
+
+/**
+ * @api {patch} /tep/resources/:resource_id/reports/:report_id/resolve Update TEP resources
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName ResolveReport
+ * @apiGroup TepResources
+ */
 router.patch('/:resource_id/reports/:report_id/resolve', resolveReport);
 
 /**
  * @api {get} /tep/resources/latest Get latest TEP resources
  * @apiHeader {String} authorization JWT Token.
- * @apiName GetLatestResources
+ * @apiName GetLatest
  * @apiGroup TepResources
  */
 router.get('/', getAll);
