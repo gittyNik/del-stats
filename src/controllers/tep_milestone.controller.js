69278708e5cbc116a258c3feb4f405cb8c7a6b20
Meghana-Chigarapalle <meghana25293@gmail.com>
2019-06-24 17:10:42 +0530

    added milestone routes

--- src/controllers/tep_milestone.controller.js
+++ src/controllers/tep_milestone.controller.js
@@ -9,6 +9,22 @@ import Resource from '../../models/resource'
 import Milestone from '../../models/milestone';
 >>>>>>> Changed the db structure of milestone and updated respective routes
 
+export const getAllMilestones=(req,res) => {
+  Milestone.findAll({})
+  .then((data) => {res.json(data);})
+  .catch(err => res.status(500).send(err));
+}
+
+export const getMilestone=(req,res) => {
+  Milestone.findAll({
+    where:{
+      id : req.params.milestone_id
+    }
+  })
+  .then((data) => {res.json(data);})
+  .catch(err => res.status(500).send(err));
+}
+
 export const create = (req, res)=> {
   let {name, topics} = req.body;
   Milestone.create({
@@ -22,6 +38,27 @@ export const create = (req, res)=> {
     });
   })
   .catch(err=>console.log(err));
+}
+
+export const update = (req,res)=>{
+  Milestone.update({name: req.body.milestone_name},{
+    where: {
+      id: req.params.milestone_id
+    }
+  })
+  .then(tepMilestone => {
+    res.send('Milestone updated');
+  })
+  .catch(err => res.status(500).send(err));
+}
+
+export const deleteOne = (req,res)=>{
+  Milestone.destroy({where: {
+      id:req.params.milestone_id
+    }
+  })
+  .then(() => {res.send("Deleted milestone");})
+  .catch(err => res.status(500).send(err));
 }
 
 export const getAllByMilestone = (req,res)=>{
--- src/routes/tep_milestone.routes.js
+++ src/routes/tep_milestone.routes.js
@@ -1,7 +1,23 @@
 import Express from 'express';
-import {create,getAllByMilestone as getMilestoneResources} from '../controllers/tep_milestone.controller';
+import {create,update,deleteOne,getAllByMilestone as getMilestoneResources,getAllMilestones,getMilestone} from '../controllers/tep_milestone.controller';
 
 const router = Express.Router();
+
+/**
+ * @api {get} /tep/milestones Get all TEP milestones
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName GetMilestones
+ * @apiGroup TEP Milestone
+ */
+router.get('/', getAllMilestones);
+
+/**
+ * @api {get} /tep/milestones/:milestone_id Get milestone by Id
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName GetMilestoneById
+ * @apiGroup TEP Milestone
+ */
+router.get('/:milestone_id', getMilestone);
 
 /**
  * @api {get} /tep/milestones/:milestone_id/resources Get TEP resources of a milestone
@@ -20,5 +36,23 @@ router.get('/:milestone_id/resources', getMilestoneResources);
  * @apiParam {String} name Name of the milestone
  */
 router.post('/', create);
+
+/**
+ * @api {patch} /tep/milestones/:milestone_id Update TEP milestone
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName Update
+ * @apiGroup TEP Milestone
+ *
+ * @apiParam {String} milestone_name name of the milestone
+ */
+router.patch('/:milestone_id', update);
+
+/**
+ * @api {delete} /tep/milestones/:milestone_id Delete TEP milestone
+ * @apiHeader {String} authorization JWT Token.
+ * @apiName DeleteOne
+ * @apiGroup TEP Milestone
+ */
+router.delete('/:milestone_id', deleteOne);
 
 export default router;
