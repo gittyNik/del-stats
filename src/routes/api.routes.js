0df1ec7b47889062f5d58dc23597be1da8c53465
Shiva Bandari <bshivagoud@gmail.com>
2019-07-03 17:48:21 +0530

    remove merge coflict errors

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -9,20 +9,12 @@ import mailer from './mailer.routes';
 import admin from './admin.routes';
 import todo from './todo.routes';
 import note from './note.routes';
-<<<<<<< HEAD
 import tepRouter from './tep.routes';
 import user from './user.routes'
 import {getProfile} from '../controllers/user.controller';
 import { browserAccessControl, devOnly } from '../controllers/access_control.controller';
 import authenticate from '../controllers/auth.controller';
 import authRouter from './auth.routes';
-=======
-import link from './link.routes';
-import user from './user.routes';
-//import timedquiz from './timedquiz.routes';
-import firewall from './firewall.routes';
-import {getProfile, populateCurrentUser} from '../controllers/user.controller';
->>>>>>> 656bd58... integrated
 
 const router = Express.Router();
 
@@ -49,17 +41,6 @@ router.use('/admin', admin);
 router.use('/todos', todo);
 router.use('/notes', note);
 router.use('/users', user)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-router.use('/timedquiz', timedquiz);
-=======
-//router.use('/timedquiz', timedquiz);
->>>>>>> e16ffd1... final models??
-router.use('/firewall', firewall);
-
-router.get('/profile', populateCurrentUser, getProfile);
->>>>>>> 656bd58... integrated
 
 router.get('/', (req, res) => res.send('API home'));
 router.use('*', (req, res) => res.sendStatus(404));
