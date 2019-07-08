3895c75a4bc64eb4e49b2623374e49c381329e3f
gilann <mad.shivam111@gmail.com>
2019-07-08 17:37:48 +0530

    made changes in firewall module's controllers and added todos in seeder

--- db/seeders/20190611101055-test_question.js
+++ db/seeders/20190611101055-test_question.js
@@ -1,4 +1,7 @@
-// todo
+// todos
+// need to make changes as per the latest db structure
+// createdAt and updatedAt are not inserted using seeders(gives not null error)
+// "id" is not generated using seeders(gives not null error)
 'use strict';
 
 var faker = require('faker');
@@ -12,67 +15,67 @@ const questionType = ['coding', 'logical', 'mindset']
 var questions = [];
 
 for(let c=1; c<=10; c++){
-    let qtype = type[Math.floor(Math.random()*type.length)];
-    if(qtype=='text'){
-        let answers = [];
-        let count = Math.floor(Math.random()*3) + 1;
-        for(let i=1; i<=count; i++){
-            answers.push(faker.lorem.sentence());
+	let qtype = type[Math.floor(Math.random()*type.length)];
+	if(qtype=='text'){
+		let answers = [];
+		let count = Math.floor(Math.random()*3) + 1;
+		for(let i=1; i<=count; i++){
+			answers.push(faker.lorem.sentence());
+		}
+		let text = {
+			type: qtype,
+			question: {
+				question: faker.lorem.sentence(),
+				image: faker.image.imageUrl(),
+				answers: answers,
+				qtype: questionType[Math.floor(Math.random()*questionType.length)],
+			}
+		}
+		questions.push(text);
+	}
+  else if(qtype=='mcq'){
+    let mcq = {
+      type: qtype,
+			question: {
+				question: faker.lorem.sentence(),
+				image: faker.image.imageUrl(),
+				options: [
+					{
+						option: faker.lorem.sentence(),
+						image: faker.image.imageUrl(),
+						isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
+					}, 
+					{
+						option: faker.lorem.sentence(),
+						image: faker.image.imageUrl(),
+						isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
+					}, 
+					{
+						option: faker.lorem.sentence(),
+						image: faker.image.imageUrl(),
+						isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
+					}, 
+					{
+						option: faker.lorem.sentence(),
+						image: faker.image.imageUrl(),
+						isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
+					}, 
+        	],
+				qtype: questionType[Math.floor(Math.random()*questionType.length)],
         }
-        let text = {
-            type: qtype,
-            question: {
-                question: faker.lorem.sentence(),
-                image: faker.image.imageUrl(),
-                answers: answers,
-                qtype: questionType[Math.floor(Math.random()*questionType.length)],
-            }
-        }
-        questions.push(text);
-    }
-    else if(qtype=='mcq'){
-        let mcq = {
-            type: qtype,
-            question: {
-                question: faker.lorem.sentence(),
-                image: faker.image.imageUrl(),
-                options: [
-                    {
-                        option: faker.lorem.sentence(),
-                        image: faker.image.imageUrl(),
-                        isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
-                    }, 
-                    {
-                        option: faker.lorem.sentence(),
-                        image: faker.image.imageUrl(),
-                        isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
-                    }, 
-                    {
-                        option: faker.lorem.sentence(),
-                        image: faker.image.imageUrl(),
-                        isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
-                    }, 
-                    {
-                        option: faker.lorem.sentence(),
-                        image: faker.image.imageUrl(),
-                        isCorrect: isCorrect[Math.floor(Math.random()*isCorrect.length)]
-                    }, 
-                ],
-                qtype: questionType[Math.floor(Math.random()*questionType.length)],
-            }
-        }
-        questions.push(mcq);
+      }
+      questions.push(mcq);
     }
     else if(qtype=='code'){
-        let code = {
-            type: qtype,
-            question: {
-                question: faker.lorem.sentence(),
-                image: faker.image.imageUrl(),
-                qtype: questionType[Math.floor(Math.random()*questionType.length)],
-            }
-        }
-        questions.push(code);
+			let code = {
+				type: qtype,
+				question: {
+					question: faker.lorem.sentence(),
+					image: faker.image.imageUrl(),
+					qtype: questionType[Math.floor(Math.random()*questionType.length)],
+				}
+			}
+			questions.push(code);
     }
 }
 
