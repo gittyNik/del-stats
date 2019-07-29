12d335f136fc74cadc5b7b3e2cb7edb3ac646036
Shiva Bandari <bshivagoud@gmail.com>
2019-07-29 23:58:34 +0530

    seed multiple demo cohorts

--- db/seeders/20190729174954-demo-cohort.js
+++ db/seeders/20190729174954-demo-cohort.js
@@ -1,17 +1,21 @@
 const uuid = require('uuid/v4');
 const faker = require('faker');
 
+const createCohort = () => ({
+  id: uuid(),
+  name: faker.lorem.word(),
+  program_id: 'demo',
+  location: faker.address.city(),
+  start_date: faker.date.future(0.5),
+  learners: [],
+  learning_ops_manager: null,
+});
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('cohorts', [{
-      id: uuid(),
-      name: faker.lorem.word(),
-      program_id: 'demo',
-      location: faker.address.city(),
-      start_date: faker.date.future(0.1),
-      learners: [],
-      learning_ops_manager: null,
-    }], {}, { learners: {type:Sequelize.ARRAY(Sequelize.UUID)}});
+    return queryInterface.bulkInsert('cohorts', [
+      createCohort(), createCohort(), createCohort()
+    ], {}, { learners: {type:Sequelize.ARRAY(Sequelize.UUID)}});
   },
 
   down: (queryInterface, Sequelize) => {
