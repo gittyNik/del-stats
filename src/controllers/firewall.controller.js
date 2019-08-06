0ba8eb7a5062db2cd0a1603dc185b3f2654d475d
Shiva Bandari <bshivagoud@gmail.com>
2019-08-06 15:04:54 +0530

    send public firewall statistics, fix #38

--- src/controllers/firewall.controller.js
+++ src/controllers/firewall.controller.js
@@ -3,10 +3,31 @@ import Application from '../models/application';
 import Program from '../models/program';
 import Cohort from '../models/cohort';
 
-export const getPublicStats = (req, res) => {
+const getUpcomingCohort = () => {
+  let tonight = new Date();
+  tonight.setHours(23);
+  tonight.setMinutes(59);
+  tonight.setSeconds(59);
+
+  return Cohort.findOne({ where: {
+    start_date: {[Sequelize.Op.gt]: tonight}
+  }})
+}
+
+export const getPublicStats = async (req, res) => {
+
+  const cohort = await getUpcomingCohort();
+
   Application.findAll()
   .then(applications => {
-    res.send({ count: applications.length});
+    const currentApplications = applications.filter(a => a.cohort_applied===cohort.id);
+    res.send({
+      cohort: {
+        total: currentApplications.length,
+        pending: currentApplications.filter(a => a.status==='review_pending').length
+      },
+      count: applications.length,
+    });
   })
   .catch(err => res.sendStatus(500))
 }
