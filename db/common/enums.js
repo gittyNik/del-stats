a0de06237f13940b4655ba9360a07f947b33e17f
Shiva Bandari <bshivagoud@gmail.com>
2019-08-20 16:04:55 +0530

    add attributes to soal_events schemas

--- db/common/enums.js
+++ db/common/enums.js
@@ -7,8 +7,11 @@ export const TEST_QUESTION_TYPE = ['mcq', 'text', 'code', 'rate', 'logo'];
 export const FIREWALL_APPLICATION_STATUS =
 ['applied', 'review_pending', 'offered', 'rejected', 'joined', 'archieved'];
 
+export const EVENT_INVITATION_STATUS = ['invited', 'rsvp', 'attended'];
+
 export default {
   domain: TEST_QUESTION_DOMAIN,
   question_type: TEST_QUESTION_TYPE,
   application_status: FIREWALL_APPLICATION_STATUS,
+  invite_status: EVENT_INVITATION_STATUS,
 };
--- db/migrations/20190820090330-create-soal-event-attendees.js
+++ db/migrations/20190820090330-create-soal-event-attendees.js
@@ -1,10 +1,25 @@
+import { EVENT_INVITATION_STATUS } from '../common/enums';
+
 const migration = {
   up: (queryInterface, Sequelize) => queryInterface.createTable('soal_event_attendees', {
     id: {
       type: Sequelize.UUID,
       primaryKey: true,
     },
+    event_id: {
+      type: Sequelize.UUID,
+      references: { model: 'soal_events', key: 'id' },
+    },
+    user_id: {
+      type: Sequelize.UUID,
+      references: { model: 'users', key: 'id' },
+    },
     name: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    email: Sequelize.STRING,
+    city: Sequelize.STRING,
+    organization: Sequelize.STRING,
+    status: Sequelize.ENUM(...EVENT_INVITATION_STATUS),
     created_at: Sequelize.DATE,
     updated_at: Sequelize.DATE,
   }),
