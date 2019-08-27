02a8684d1f3a92cdf84d6e3162681ec8a617decc
Shiva Bandari <bshivagoud@gmail.com>
2019-08-27 14:30:28 +0530

    add attributes to assessment schemas

--- db/migrations/20190820091004-create-cohort-assessments.js
+++ db/migrations/20190820091004-create-cohort-assessments.js
@@ -4,7 +4,20 @@ const migration = {
       type: Sequelize.UUID,
       primaryKey: true,
     },
-    name: Sequelize.STRING,
+    cohort_id: {
+      type: Sequelize.UUID,
+      references: { model: 'cohorts', key: 'id' },
+    },
+    assessment_id: {
+      type: Sequelize.UUID,
+      references: { model: 'assessments', key: 'id' },
+    },
+    catalyst_id: {
+      type: Sequelize.UUID,
+      references: { model: 'users', key: 'id' },
+    },
+    location: Sequelize.STRING,
+    time_scheduled: Sequelize.DATE,
     created_at: Sequelize.DATE,
     updated_at: Sequelize.DATE,
   }),
