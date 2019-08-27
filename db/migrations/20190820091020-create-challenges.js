a9846b371fb45b17b38097f5bc19454ff256d691
Shiva Bandari <bshivagoud@gmail.com>
2019-08-27 14:40:11 +0530

    add attributes to challenge schemas

--- db/migrations/20190820091020-create-challenges.js
+++ db/migrations/20190820091020-create-challenges.js
@@ -1,10 +1,18 @@
+import { CHALLENGE_DIFFICULTY, CHALLENGE_SIZE } from '../common/enums';
 const migration = {
   up: (queryInterface, Sequelize) => queryInterface.createTable('challenges', {
     id: {
       type: Sequelize.UUID,
       primaryKey: true,
     },
-    name: Sequelize.STRING,
+    topic_id: {
+      type: Sequelize.UUID,
+      references: { model: 'topics', key: 'id' },
+    },
+    description: Sequelize.TEXT,
+    starter_repo: Sequelize.STRING,
+    difficulty: Sequelize.ENUM(...CHALLENGE_DIFFICULTY),
+    size: Sequelize.ENUM(...CHALLENGE_SIZE),
     created_at: Sequelize.DATE,
     updated_at: Sequelize.DATE,
   }),
--- db/migrations/20190820091027-create-learner-challenges.js
+++ db/migrations/20190820091027-create-learner-challenges.js
@@ -4,7 +4,21 @@ const migration = {
       type: Sequelize.UUID,
       primaryKey: true,
     },
-    name: Sequelize.STRING,
+    challenge_id: {
+      type: Sequelize.UUID,
+      references: { model: 'challenges', key: 'id' },
+    },
+    learner_id: {
+      type: Sequelize.UUID,
+      references: { model: 'users', key: 'id' },
+    },
+    repo: Sequelize.STRING,
+    learner_feedback: Sequelize.TEXT,
+    review: Sequelize.TEXT,
+    reviewed_by: {
+      type: Sequelize.UUID,
+      references: { model: 'users', key: 'id' },
+    },
     created_at: Sequelize.DATE,
     updated_at: Sequelize.DATE,
   }),
