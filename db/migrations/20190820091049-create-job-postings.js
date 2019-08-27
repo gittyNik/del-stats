f7c90e9949609f1461720d12552fbb24b7b6676b
Shiva Bandari <bshivagoud@gmail.com>
2019-08-27 14:54:08 +0530

    add attributes to career portal schemas

--- db/migrations/20190820091049-create-job-postings.js
+++ db/migrations/20190820091049-create-job-postings.js
@@ -1,10 +1,24 @@
+import { JOB_TYPE, JOB_POSTING_EXCLUSIVITY} from '../common/enums';
 const migration = {
   up: (queryInterface, Sequelize) => queryInterface.createTable('job_postings', {
     id: {
       type: Sequelize.UUID,
       primaryKey: true,
     },
-    name: Sequelize.STRING,
+    title: Sequelize.STRING,
+    company: Sequelize.STRING,
+    description: Sequelize.TEXT,
+    job_type: Sequelize.ENUM(...JOB_TYPE),
+    salary_range: Sequelize.STRING,
+    industry: Sequelize.STRING,
+    locations: Sequelize.ARRAY(Sequelize.STRING),
+    posted_by: {
+      type: Sequelize.UUID,
+      references: { model: 'users', key: 'id' },
+    },
+    contact_person_details: Sequelize.JSON,
+    experience_required: Sequelize.STRING,
+    exclusivity: Sequelize.ENUM(...JOB_POSTING_EXCLUSIVITY),
     created_at: Sequelize.DATE,
     updated_at: Sequelize.DATE,
   }),
