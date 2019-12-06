8c9ef1ec394d1a1d93dff50d84aafe8cb76762ba
velisoju pranay <velisojupranay@gmail.com>
2019-12-06 12:09:24 +0530

    Updated team.js model

--- src/models/team.js
+++ src/models/team.js
@@ -11,6 +11,21 @@ export const Team = db.define('teams', {
     allowNull: false,
     type: Sequelize.STRING,
   },
+  cohort_milestone_id: {
+    type: Sequelize.UUID,
+    references: { model: 'cohort_milestone', key: 'id' },
+  },
+  learners: Sequelize.ARRAY(Sequelize.UUID),
+  github_repo_link: Sequelize.STRING,
+  product_demo_link: Sequelize.STRING,
+  review: Sequelize.TEXT,
+  review_by: {
+    type: Sequelize.UUID,
+    references: { model: 'users', key: 'id' },
+  },
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE,
+
 });
 
 export default Team;
