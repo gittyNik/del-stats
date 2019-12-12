4f3761bd54462fa6f475e3ee055939ec2e12871d
velisoju pranay <velisojupranay@gmail.com>
2019-12-12 14:27:55 +0530

    Added middleware allowAdminsOnly to nudge and ops routes.

--- src/routes/learning/index.js
+++ src/routes/learning/index.js
@@ -5,13 +5,14 @@ import nudge from './nudge.routes';
 import ops from './ops.routes';
 import cohortRouter from './cohort.routes';
 import programRouter from './program.routes';
+import { allowAdminsOnly } from '../../controllers/auth/roles.controller';
 
 const router = Express.Router();
 
 router.use('/activity', activity);
 router.use('/content', content);
-router.use('/nudge', nudge);
-router.use('/ops', ops);
+router.use('/nudge', allowAdminsOnly, nudge);
+router.use('/ops', allowAdminsOnly, ops);
 
 router.use('/cohorts', cohortRouter);
 router.use('/programs', programRouter);
