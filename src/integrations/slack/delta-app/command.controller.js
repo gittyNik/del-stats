23ef92e73249ffcdb8c4fc2137382ff1a331398b
Shiva Bandari <bshivagoud@gmail.com>
2019-12-23 17:01:10 +0530

    handle slack registration failure correctly

--- src/integrations/slack/delta-app/command.controller.js
+++ src/integrations/slack/delta-app/command.controller.js
@@ -29,7 +29,7 @@ const authenticate = (req, res, next) => {
     });
 };
 
-const registerSlack = (slack_user_id) => web.users.info({ user })
+const registerSlack = (slack_user_id) => web.users.info({ user: slack_user_id })
   // fetch the profile from slack server
   .then(response => response.user.profile)
   // find the user
@@ -38,12 +38,12 @@ const registerSlack = (slack_user_id) => web.users.info({ user })
 
     return User.findOne({ where: { email, phone } })
       .then(user => {
+        if (user === null) {
+          console.log(email, phone);
+          return Promise.reject(new Error('User not found'));
+        }
         console.log('User matched!');
         return { user, profile };
-      })
-      .catch(err => {
-        console.log(err);
-        return Promise.reject(new Error('User not found'));
       });
   })
   // save the social connection
@@ -57,8 +57,10 @@ const registerSlack = (slack_user_id) => web.users.info({ user })
 
 // User registration
 router.use((req, res, next) => {
-  const { text, user_id, command } = req.body;
-  if (command === 'delta' && text === 'register') {
+  const {
+    command, text, user_id, user_name,
+  } = req.body;
+  if (command === '/delta' && text === 'register') {
     registerSlack(user_id)
       .then(social_connection => {
         console.log(social_connection);
@@ -66,7 +68,7 @@ router.use((req, res, next) => {
       })
       .catch(err => {
         console.error(err);
-        res.sendStatus(404);
+        res.send(`Registration failed for @${user_name} !`);
       });
   } else {
     next();
