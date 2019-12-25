6c34ce4a68bd655ea60b42da3a8dde5dc5cd0fc8
Shiva Bandari <bshivagoud@gmail.com>
2019-12-25 13:21:06 +0530

    notify moderators on saving the link

--- src/integrations/slack/delta-app/controllers/resource.controller.js
+++ src/integrations/slack/delta-app/controllers/resource.controller.js
@@ -1,4 +1,13 @@
 import { createFromSlackAttachment } from '../../../../models/resource';
+import web from '../client';
+
+export const notifyModerator = resource => {
+  console.log(resource);
+  return web.chat.postMessage({
+    text: 'Link added to delta',
+    channel: 'resources',
+  });
+};
 
 export const saveLink = (payload, respond, authData) => {
   // Logs the contents of the action to the console
@@ -7,7 +16,11 @@ export const saveLink = (payload, respond, authData) => {
     createFromSlackAttachment(attachments[0], authData.user_id)
       .then(resource => {
         const text = 'Thank you for your help! The link is queued for approval.';
-        respond({ text }).catch(e => console.error(e));
+        Promise.all([
+          notifyModerator(resource),
+          respond({ text }),
+        ])
+          .catch(e => console.error(e));
       })
       .catch(err => {
         let text;
