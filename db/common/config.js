564ac43a56f970c372466e65b28642131fb40409
Shiva Bandari <bshivagoud@gmail.com>
2020-01-09 18:15:27 +0530

    configure test environment

--- db/common/config.js
+++ db/common/config.js
@@ -1,41 +1,54 @@
 require('dotenv').config();
 require('@babel/register');
 
+const common = {
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    acquire: 30000,
+    idle: 10000,
+  },
+  define: {
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
+
 const settings = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT || 5432,
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      acquire: 30000,
-      idle: 10000,
-    },
-    define: {
-      timestamps: false,
-      underscored: true,
-      underscoredAll: true,
-    },
-  },
+    username: process.env.DEV_DB_USERNAME || 'postgres',
+    password: process.env.DEV_DB_PASSWORD || '',
+    database: process.env.DEV_DB_NAME || 'delta_development',
+    host: process.env.DEV_DB_HOST || '127.0.0.1',
+    port: process.env.DEV_DB_PORT || 5432,
+    ...common
+ },
   test: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
+    username: process.env.TEST_DB_USERNAME || 'postgres',
+    password: process.env.TEST_DB_PASSWORD || '',
+    database: process.env.TEST_DB_NAME || 'delta_test',
+    host: process.env.TEST_DB_HOST || '127.0.0.1',
+    port: process.env.TEST_DB_PORT || 5432,
+    ...common
   },
   production: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
+    port: process.env.DATABASE_PORT || 5432,
+    ...common
   },
 };
 
-settings.default = settings.development;
+switch(process.env.NODE_ENV) {
+  case 'test':
+  case 'production':
+    settings.default = settings[process.env.NODE_ENV];
+    break;
+  default:
+    settings.default = settings.development;
+}
 
 module.exports = settings;
