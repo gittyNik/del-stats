b94dc5f4e6ef06d8f36fce08afa5e0abc319f169
Shiva Bandari <bshivagoud@gmail.com>
2020-01-10 15:57:44 +0530

    add all models to index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,73 @@
-import { User } from './user';
-import { Cohort } from './cohort';
+import { Application } from 'application';
+import { Assessment } from 'assessment';
+import { BrowsreHistoryItem } from 'browser_history_items';
+import { BrowsreVisitItem } from 'browser_visit_items';
+import { Challenge } from 'challenge';
+import { CohortAssessment } from 'cohort_assessment';
+import { CohortBreakout } from 'cohort_breakout';
+import { Cohort } from 'cohort';
+import { CohortMilestone } from 'cohort_milestone';
+import { ConfigParam } from 'config_param';
+import { JobApplication } from 'job_application';
+import { LearnerAssessment } from 'learner_assessment';
+import { LearnerBreakout } from 'learner_breakout';
+import { LearnerChallenge } from 'learner_challenge';
+import { Milestone } from 'milestone';
+import { Note } from 'note';
+import { Ping } from 'ping';
+import { PingTemplate } from 'ping_template';
+import { Pong } from 'pong';
+import { Portfolio } from 'portfolio';
+import { Program } from 'program';
+import { Progress } from 'progress';
+import { ResourceComment } from 'resource_comment';
+import { Resource } from 'resource';
+import { ResourceReport } from 'resource_report';
+import { ResourceVisit } from 'resource_visit';
+import { ResourceVote } from 'resource_vote';
+import { SocialConnection } from 'social_connection';
+import { Team } from 'team';
+import { Test } from 'test';
+import { TestQuestion } from 'test_question';
+import { Todo } from 'todo';
+import { Topic } from 'topic';
+import { User } from 'user';
 
+// TODO: degine all associations here
 
 export default {
-  User, Cohort,
+  Application,
+  Assessment,
+  BrowsreHistoryItem,
+  BrowsreVisitItem,
+  Challenge,
+  CohortAssessment,
+  CohortBreakout,
+  Cohort,
+  CohortMilestone,
+  ConfigParam,
+  JobApplication,
+  LearnerAssessment,
+  LearnerBreakout,
+  LearnerChallenge,
+  Milestone,
+  Note,
+  Ping,
+  PingTemplate,
+  Pong,
+  Portfolio,
+  Program,
+  Progress,
+  ResourceComment,
+  Resource,
+  ResourceReport,
+  ResourceVisit,
+  ResourceVote,
+  SocialConnection,
+  Team,
+  Test,
+  TestQuestion,
+  Todo,
+  Topic,
+  User,
 };
