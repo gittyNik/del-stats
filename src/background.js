9bb731a7a5ad75241004b7a090a77aa263510a91
Shiva Bandari <bshivagoud@gmail.com>
2020-01-13 22:20:33 +0530

    create few cron jobs and add to queue

--- src/background.js
+++ src/background.js
@@ -1,9 +1,30 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { Worker } from 'bullmq';
+import { initQueue } from './controllers/queue.controller';
 
-const worker = new Worker('delta', job => {
+const deltaHandler = job => {
+  console.log(job);
   if (job.name === 'breakouts') {
     console.log('sending breakouts schedule for the day');
   }
+
+  if (job.name === 'slack_firewall_stats') {
+    console.log('sending firewall stats from previous day');
+  }
+};
+
+const worker = new Worker('delta', deltaHandler);
+
+initQueue();
+
+worker.on('completed', (job) => {
+  console.log(`${job.id} has completed!`);
 });
+
+worker.on('failed', (job, err) => {
+  console.log(`${job.id} has failed with ${err.message}`);
+});
+
+
+initQueue();
--- src/controllers/queue.controller.js
+++ src/controllers/queue.controller.js
@@ -1,7 +1,15 @@
 import { Queue } from 'bullmq';
 
 const myQueue = new Queue('delta');
+const everyMorning = { cron: '0 7 * * *' };
+const everySecond = { cron: '* * * ? * *' };
 
 export const sendBreakoutSchedule = () => {
   myQueue.add('breakouts', { topic: 'intro to node' });
 };
+
+const slackFirewallStats = () => myQueue.add('slack_firewall_stats', {}, {
+  repeat: everySecond,
+});
+
+export const initQueue = () => slackFirewallStats();
