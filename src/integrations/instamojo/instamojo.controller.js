64cfee9ef72dad997c9ad9af999353779430d3ae
Pranay Velisoju <pranay@soal.io>
2020-01-18 03:26:41 +0530

    webhook response are stored in an array in db

--- src/integrations/instamojo/instamojo.controller.js
+++ src/integrations/instamojo/instamojo.controller.js
@@ -1,13 +1,42 @@
+import { Application } from '../../models/application';
 
 export const instamojo_webhook = (req, res) => {
-  const {
-    amount, buyer, buyer_name, buyer_phone, fees,
-    currency, longurl, mac, payment_id,
-    payment_request_id, purpose, shorturl, status,
-  } = req.body;
-  // todo:  handle this data
-  console.log(req.body);
-  res.sendStatus(200);
+  const { purpose } = req.body;
+
+  Application.findAll({
+    where: { id: purpose }, returning: true, plain: true,
+  })
+    .then(result => {
+      // console.log(result.dataValues.payment_details);
+      let old_payment_details = result.dataValues.payment_details;
+      let res_data = old_payment_details.response_data;
+      // console.log('old response_data:', res_data);
+      res_data.push(req.body);
+      // console.log('new response_data: ', res_data);
+      old_payment_details.response_data = res_data;
+      let payment_details = old_payment_details;
+
+      Application.update({
+        payment_details,
+      }, { where: { id: purpose }, returning: true, plain: true })
+        .then(result2 => {
+          // console.log(result2[1].dataValues);
+          // console.log(result2[1].dataValues.payment_details.response_data);
+          res.status(200).send({
+            text: 'response data from webhook',
+            // An array of all the webhook responses.
+            data: result2[1].dataValues.payment_details.response_data,
+          });
+        })
+        .catch(err => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 export default instamojo_webhook;
