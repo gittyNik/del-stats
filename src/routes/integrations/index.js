126cc95cdf67bd4affb84f8074693c816d4df524
Pranay Velisoju <pranay@soal.io>
2020-02-12 21:30:32 +0530

    skeleton for zoom integration

--- src/routes/integrations/index.js
+++ src/routes/integrations/index.js
@@ -2,7 +2,11 @@ import Express from 'express';
 import slackRouter from './slack';
 import instamojoRouter from './instamojo';
 import hubspotRouter from './hubspot';
+<<<<<<< HEAD
 import githubRouter from './github';
+=======
+import zoomRouter from './zoom';
+>>>>>>> 4a06a20... skeleton for zoom integration
 import { send404, browserAccessControl } from '../../controllers/api.controller';
 
 const router = Express.Router();
@@ -13,7 +17,11 @@ router.use(browserAccessControl);
 router.use('/slack', slackRouter);
 router.use('/instamojo', instamojoRouter);
 router.use('/hubspot', hubspotRouter);
+<<<<<<< HEAD
 router.use('/github', githubRouter);
+=======
+router.use('/zoom', zoomRouter);
+>>>>>>> 4a06a20... skeleton for zoom integration
 router.use('*', send404);
 
 export default router;
