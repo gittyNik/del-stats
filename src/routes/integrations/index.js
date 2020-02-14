9eefb4a7903e459b354e776ac48f6480556e8c3f
Pranay Velisoju <pranay@soal.io>
2020-02-14 11:11:19 +0530

    merged zoom and codesanbox basic branches

--- src/routes/integrations/index.js
+++ src/routes/integrations/index.js
@@ -3,6 +3,7 @@ import slackRouter from './slack';
 import instamojoRouter from './instamojo';
 import hubspotRouter from './hubspot';
 import zoomRouter from './zoom';
+import codeSandboxRouter from './code_sandbox';
 import { send404, browserAccessControl } from '../../controllers/api.controller';
 
 const router = Express.Router();
@@ -14,6 +15,7 @@ router.use('/slack', slackRouter);
 router.use('/instamojo', instamojoRouter);
 router.use('/hubspot', hubspotRouter);
 router.use('/zoom', zoomRouter);
+router.use('/code', codeSandboxRouter);
 router.use('*', send404);
 
 export default router;
