38ba610536052e636efd1c1c8bf1ed7da57e0eea
vaulstein <vaulstein@gmail.com>
2020-05-23 11:31:57 +0530

    Added route to create learner breakouts for particular Cohort

--- src/controllers/learning/learner_breakout.controller.js
+++ src/controllers/learning/learner_breakout.controller.js
@@ -1,5 +1,5 @@
 import { uuid } from 'uuid/v4';
-import { LearnerBreakout } from '../../models/learner_breakout';
+import { LearnerBreakout, createLearnerBreakouts } from '../../models/learner_breakout';
 import { CohortBreakout } from '../../models/cohort_breakout';
 import { User } from '../../models/user';
 
@@ -7,6 +7,17 @@ export const getLearnerBreakouts = (req, res) => {
   LearnerBreakout.findAll({
     attributes: ['cohort_breakout_id'],
   })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.error(err.stack);
+      res.status(500);
+    });
+};
+
+export const learnerBreakoutsCreate = (req, res) => {
+  const { learner_id, cohort_id } = req.body;
+
+  createLearnerBreakouts(learner_id, cohort_id)
     .then(data => res.json(data))
     .catch(err => {
       console.error(err.stack);
@@ -49,16 +60,16 @@ export const createLearnerBreakout = (req, res) => {
 };
 
 export const getLearnerBreakoutsByBreakoutId = (req, res) => {
-  const {cohort_breakout_id} = req.params;
+  const { cohort_breakout_id } = req.params;
   LearnerBreakout.findAll({
     where: {
-      cohort_breakout_id
+      cohort_breakout_id,
     },
-    include: [User]
+    include: [User],
   })
     .then(data => res.json({
-      text: "Learner breakouts for a cohort breakout",
-      data
+      text: 'Learner breakouts for a cohort breakout',
+      data,
     }))
     .catch(err => {
       console.error(err.stack);
@@ -68,20 +79,20 @@ export const getLearnerBreakoutsByBreakoutId = (req, res) => {
 
 export const markAttendance = (req, res) => {
   const {
-    learnerBreakouts
+    learnerBreakouts,
   } = req.body;
   Promise.all(learnerBreakouts.map(breakout => LearnerBreakout.update({
-    attendance: breakout.attendance
+    attendance: breakout.attendance,
   }, {
     where: {
-      id: breakout.id
-    }
+      id: breakout.id,
+    },
   }))).then(() => {
     res.json({
-      text: "Mark attendance success"
-    })
+      text: 'Mark attendance success',
+    });
   }).catch(err => {
     console.error(err.stack);
     res.status(500);
   });
-}
+};
