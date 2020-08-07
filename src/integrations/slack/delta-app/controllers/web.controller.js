646cd4634ab5702696ad5efb71b35b7c93b9d0c9
Pranay Velisoju <pranay@soal.io>
2020-08-07 19:10:27 +0530

    notify learner or cohort for review, assessment, breakout and question hour

--- src/integrations/slack/delta-app/controllers/web.controller.js
+++ src/integrations/slack/delta-app/controllers/web.controller.js
@@ -6,6 +6,11 @@ import { CohortBreakout } from "../../../../models/cohort_breakout";
 import { postMessage } from '../utility/chat';
 import { getChannelIdForCohort } from '../../../../models/slack_channels';
 
+const REVIEW_TEMPLATE = (team_number) => `Team: ${team_number} join the review`;
+const ASSESSMENT_TEMPLATE = 'Join the Assement now';
+const BREAKOUT_TEMPLATE = 'Join the Breakout now';
+const QUESTIONAIRE_TEMPLATE = 'Join the Question Hour now';
+
 export const sendMessage = (req, res) => {
   const { userId, cohortId, breakoutId, topics } = req.body;
   Promise.all([
@@ -29,19 +34,37 @@ export const sendMessage = (req, res) => {
 };
 
 export const notifyLearnerInChannel = async (req, res) => {
-  const { learner_id, text } = req.body;
+  let { learner_id, text, cohort_id, type, team_number } = req.body;
   const learner = await getProfile(learner_id);
   const { email } = learner;
   let slackUserResponse;
   try {
     slackUserResponse = await web.users.lookupByEmail({ email });
     const slackUserId = slackUserResponse.user.id;
-    const cohort_id = await getCohortIdFromLearnerId(learner_id);
+    if (typeof cohort_id === 'undefined') {
+      cohort_id = await getCohortIdFromLearnerId(learner_id);
+    }
     const channel_id = await getChannelIdForCohort(cohort_id);
     // console.log(channel_id);
-    const updatedText = `<@${slackUserId}> ${text}`;
+
+    switch (type) {
+      case 'review':
+        text = REVIEW_TEMPLATE(team_number);
+        break;
+      case 'assessment':
+        text = ASSESSMENT_TEMPLATE;
+        break;
+      case 'breakout':
+        text = BREAKOUT_TEMPLATE;
+        break;
+      case 'question_hour':
+        text = QUESTIONAIRE_TEMPLATE;
+        break;
+      // no default
+    }
+    const updatedText = (req.body.cohort_id) ? `<!channel> ${text}` : `<@${slackUserId}> ${text}`;
     const post_res = await postMessage({ channel: channel_id, text: updatedText });
-    res.status(200).json({
+    return res.status(200).json({
       text: 'Message posted on the channel',
       data: {
         channel: post_res.channel,
@@ -51,8 +74,8 @@ export const notifyLearnerInChannel = async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({
-      text: 'Failed to fetch slack user id from email',
+    return res.status(500).json({
+      text: 'Failed to notify on the slack channel',
     });
   }
 };
