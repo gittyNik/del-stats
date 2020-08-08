1ca6d77d63b61c9b781c60a590a2c2dd7210824c
Pranay Velisoju <pranay@soal.io>
2020-08-08 14:50:29 +0530

    authenticated the slack route

--- src/integrations/slack/delta-app/routes/webRouter.js
+++ src/integrations/slack/delta-app/routes/webRouter.js
@@ -1,14 +1,20 @@
 import Express from 'express';
 import { sendMessage, notifyLearnersInChannel } from '../controllers/web.controller';
+import authenticate from '../../../../controllers/auth/auth.controller';
 
 const router = Express.Router();
 
 router.post('/send-message', sendMessage);
+
+// Private Routes.
+
+router.use(authenticate);
 /*
  * @api {post} /integrations/slack/delta/web/notify-learners  Notify learners in slack
  * @apiDescription Notify learner to join the breakout or review or assessment or QH
  * @apiName NotifyLearnersInSlackChannel
  * @apiGroup SlackIntegration
+ * @apiHeader {String} authorization JWT Token.
  *
  * @apiParam {String} cohort_id to notify using @channel.
  * @apiParam {String} learner_id Id of the learner.
