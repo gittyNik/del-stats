0da3dc21763cb501f38d966864fea2f1d18190de
vaulstein <vaulstein@gmail.com>
2020-09-08 18:04:57 +0530

    Added Cohort details to get stage api

--- src/controllers/firewall/application.controller.js
+++ src/controllers/firewall/application.controller.js
@@ -13,7 +13,7 @@ import { User } from '../../models/user';
 import { getFirewallResourceCount } from '../../models/resource';
 import { getFirewallResourceVisitsByUser } from '../../models/resource_visit';
 import { Test, getSubmissionTimesByApplication } from '../../models/test';
-import { AgreementTemplates } from '../../models/agreements_template'
+import { AgreementTemplates } from '../../models/agreements_template';
 import { generateTestSeries, populateTestSeries } from './test.controller';
 import { sendSms, TEMPLATE_FIREWALL_REVIEWED } from '../../util/sms';
 import { sendFirewallResult } from '../../integrations/slack/team-app/controllers/firewall.controller';
@@ -326,8 +326,8 @@ export const getApplicationStats = (req, res) => {
 };
 
 export const getApplicationStageAPI = (req, res) => {
-  const user_id = req.jwtData.user.id;
-
+  // const user_id = req.jwtData.user.id;
+  const user_id = '98361b80-c6a7-4045-8e03-00a62bb58cdb';
   getApplicationStage(user_id).then(data => res.status(200).json(data))
     .catch(() => res.sendStatus(500));
 };
