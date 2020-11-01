6da8e45d7615fc34fcb28cb7d2b6fded6d0f4322
Pranay Velisoju <pranay@soal.io>
2020-11-01 19:26:06 +0530

    Real migration for portfolio

--- db/migrations/20201101111350-update-portfolio.js
+++ db/migrations/20201101111350-update-portfolio.js
@@ -1,10 +1,12 @@
 const migration = {
   up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(t => Promise.all([
-    queryInterface.changeColumn('portfolios', 'showcase_projects', { type: Sequelize.ARRAY(Sequelize.JSON) }, { transaction: t }),
+    queryInterface.removeColumn('portfolios', 'showcase_projects', { transaction: t }),
+    queryInterface.addColumn('portfolios', 'showcase_projects', { type: Sequelize.ARRAY(Sequelize.JSON) }, { transaction: t }),
   ])),
 
   down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(t => Promise.all([
-    queryInterface.changeColumn('portfolios', 'showcase_projects', { type: Sequelize.ARRAY(Sequelize.STRING) }, { transaction: t }),
+    queryInterface.removeColumn('portfolios', 'showcase_projects', { transaction: t }),
+    queryInterface.addColumn('portfolios', 'showcase_projects', Sequelize.ARRAY(Sequelize.STRING), { transaction: t }),
   ])),
 };
 
