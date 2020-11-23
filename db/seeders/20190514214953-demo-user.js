f650432c4bf01f775e6bdbbde84ab2776f2dcace
Ayush <Ayush-15@live.com>
2020-11-23 16:43:07 +0530

    users seeder

--- db/seeders/20190514214953-demo-user.js
+++ db/seeders/20190514214953-demo-user.js
@@ -1,12 +1,27 @@
 import uuid from 'uuid/v4';
+import faker from 'faker';
+import _ from 'lodash';
+
+const USER_ROLES = ['learner', 'educator', 'enabler', 'catalyst', 'admin',
+  'guest', 'superadmin', 'reviewer', 'operations', 'recruiter', 'career-services'];
+
+const USER = () => ({
+  id: uuid(),
+  name: faker.name.firstName(),
+  email: faker.internet.email(),
+  phone: faker.phone.phoneNumber('+91##########'),
+  role: _.sample(USER_ROLES),
+  location: faker.address.city(),
+  // picture: faker.internet.avatar(), not migrated on table
+});
 
 const seeder = {
-  up: queryInterface => queryInterface.bulkInsert('users', [{
-    id: uuid(),
-    name: 'John Doe',
-    email: 'john@doe.com',
-    phone: '9876543210',
-  }], {}),
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
+    'users', _.times(100, USER),
+    {
+      profile: { type: Sequelize.JSON() },
+    },
+  ),
 
   down: queryInterface => queryInterface.bulkDelete('users', null, {}),
 };
