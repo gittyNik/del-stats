f99496062100266e5594653a9fbe66bd6a835b4b
Ayush <Ayush-15@live.com>
2020-11-23 16:50:27 +0530

    topics seeder

---
+++ db/seeders/20201122183052-demo-topics.js
@@ -0,0 +1,59 @@
+import uuid from 'uuid/v4';
+import faker from 'faker';
+import _ from 'lodash';
+import { randomNum, generateUuids } from '../../src/util/seederUtils';
+
+const MILESTONE = {
+  id: uuid(),
+  name: faker.company.companyName(),
+  // duration: randomNum(10),
+  // alias: faker.lorem.word(),
+  prerequisite_milestones: generateUuids(5),
+  problem_statement: faker.lorem.paragraph(),
+  // learning_competencies: [''],
+  guidelines: faker.lorem.text(),
+  starter_repo: faker.internet.domainName(),
+  // releases: {},
+  created_at: new Date(),
+  updated_by: null,
+};
+
+const TOPIC = () => ({
+  id: uuid(),
+  title: faker.lorem.sentence(randomNum(6)),
+  description: faker.lorem.text(),
+  program: 'demo',
+  milestone_id: MILESTONE.id,
+  optional: false,
+  visible: true,
+  domain: _.sample(['generic', 'tech', 'mindset', 'dsa']),
+  created_at: new Date(),
+  updated_at: new Date(),
+  // path: _.sample(BREAKOUT_PATH),
+});
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async (t) => {
+    const addMilestones = queryInterface.bulkInsert(
+      'milestones',
+      [MILESTONE], { transaction: t },
+      { releases: { type: Sequelize.JSON() } },
+    );
+
+    const addTopics = queryInterface.bulkInsert(
+      'topics',
+      _.times(100, TOPIC), { transaction: t },
+    );
+
+    return Promise.all([addMilestones, addTopics])
+      .then(() => console.log('Seeded Topics'))
+      .catch(err => console.error(err));
+  }),
+
+  down: async queryInterface => queryInterface.sequelize.transaction(t => Promise.all([
+    queryInterface.bulkDelete('milestones', null, { transaction: t }),
+    queryInterface.bulkDelete('topics', null, { transaction: t }),
+  ])
+    .then(() => console.log('milestones and topics reverted'))
+    .catch(err => console.error(err))),
+};
