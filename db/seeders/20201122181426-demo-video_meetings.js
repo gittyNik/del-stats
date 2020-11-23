09148a93753d242b5e6276766bd2856845fab8c5
Ayush <Ayush-15@live.com>
2020-11-23 16:52:59 +0530

    video_meetings seeders

---
+++ db/seeders/20201122181426-demo-video_meetings.js
@@ -0,0 +1,24 @@
+import uuid from 'uuid/v4';
+import faker from 'faker';
+import _ from 'lodash';
+
+const VIDEO_MEETING = () => ({
+  id: uuid(),
+  video_id: faker.internet.domainName(),
+  start_url: faker.internet.domainName(),
+  join_url: faker.internet.domainName(),
+  start_time: String(faker.date.future()), // zoom time format
+  duration: '1 Hour',
+  zoom_user: faker.name.firstName(),
+  created_at: new Date(),
+  updated_at: new Date(),
+});
+
+module.exports = {
+  up: (queryInterface) => queryInterface.bulkInsert(
+    'video_meetings', _.times(100, VIDEO_MEETING),
+    {},
+  ),
+
+  down: queryInterface => queryInterface.bulkDelete('video_meetings', null, {}),
+};
