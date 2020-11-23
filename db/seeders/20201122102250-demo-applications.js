054069d4377e191163006748a0ae5097c462fc71
Ayush <Ayush-15@live.com>
2020-11-23 16:51:32 +0530

    applications seeders

---
+++ db/seeders/20201122102250-demo-applications.js
@@ -0,0 +1,82 @@
+import uuid from 'uuid/v4';
+import faker from 'faker';
+import _ from 'lodash';
+
+const USER = {
+  id: uuid(),
+  name: faker.name.firstName(),
+  email: faker.internet.email(),
+  phone: faker.phone.phoneNumber('+91##########'),
+  role: 'learner',
+  location: faker.address.city(),
+  // picture: faker.internet.avatar(), not migrated on table
+};
+
+const APPLICATION_STAGE = [
+  'firewall-test', 'isa-selection', 'cohort-selection_payment',
+  'cohort-selection_job-guarantee',
+  'cohort-selection-loan', 'verification-digio-document-creation',
+  'verification-digio-esign',
+  'verification-document-upload', 'verification-document-verified',
+  'payment_full',
+  'authorisation-github', 'authorisation-zoom',
+];
+
+const seeder = {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async (t) => {
+    const addUser = queryInterface.bulkInsert(
+      'users', [USER],
+      { transaction: t },
+      {
+        profile: { type: new Sequelize.JSON() },
+        status: { type: new Sequelize.ARRAY(Sequelize.JSON) },
+        status_reason: { type: new Sequelize.ARRAY(Sequelize.JSON) },
+      },
+    );
+
+    const cohorts = await queryInterface.sequelize.query(
+      `SELECT id from cohorts;`
+    );
+
+    const APPLICATION = () => ({
+      id: uuid(),
+      user_id: USER.id,
+      cohort_applied: (_.sample(cohorts[0])).id,
+      cohort_joining: (_.sample(cohorts[0])).id,
+      status: _.sample(['applied',
+        'review_pending',
+        'offered',
+        'rejected',
+        'joined',
+        'archieved']),
+      stage: _.sample(...APPLICATION_STAGE),
+      is_isa: _.sample([true, false]),
+      is_job_guarantee: _.sample([true, false]),
+      created_at: new Date(),
+      updated_at: new Date(),
+      payment_type: _.sample(['cash', 'cheque']),
+    });
+
+    const addApplications = queryInterface.bulkInsert(
+      'applications',
+      _.times(100, APPLICATION), { transaction: t },
+      {
+        releases: { type: Sequelize.JSON() },
+        payment_details: { type: Sequelize.JSON() },
+      },
+    );
+
+    return Promise.all([addUser, addApplications])
+      .then(() => console.log('Seeded Applications'))
+      .catch(err => console.error(err));
+  }),
+
+  down: async queryInterface => queryInterface.sequelize.transaction(t => Promise.all([
+    queryInterface.bulkDelete('users', null, { transaction: t }),
+    queryInterface.bulkDelete('applications', null, { transaction: t }),
+  ])
+    .then(() => console.log('users and applications reverted'))
+    .catch(err => console.error(err))),
+};
+
+export default seeder;
