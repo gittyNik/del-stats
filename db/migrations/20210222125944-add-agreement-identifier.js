5f0fb64ba8e3aff738a200d69b319cfee159d92c
vaulstein <vaulstein@gmail.com>
2021-02-22 18:43:36 +0530

    Updated migration

--- db/migrations/20210222125944-add-agreement-identifier.js
+++ db/migrations/20210222125944-add-agreement-identifier.js
@@ -1,12 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(t => Promise.all([
-    queryInterface.addColumn('agreement_templates', 'is_isa', {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
+    queryInterface.addColumn('agreement_templates', 'agreement_identifier', {
+      type: Sequelize.STRING,
+      allowNull: false,
     }, { transaction: t }),
   ])),
 
   down: (queryInterface) => queryInterface.sequelize.transaction(t => Promise.all([
-    queryInterface.removeColumn('agreement_templates', 'is_isa', { transaction: t }),
+    queryInterface.removeColumn('agreement_templates', 'agreement_identifier', { transaction: t }),
   ])),
 };
