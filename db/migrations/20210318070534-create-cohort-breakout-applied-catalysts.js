9da8cd76b6bf0e42cb7ff024542e5d004ff32d72
Niskarsh <niskarshk@gmail.com>
2021-03-18 12:41:01 +0530

    DEL-261
    Created model for association between catalyst breakout and applied catalyst

---
+++ db/migrations/20210318070534-create-cohort-breakout-applied-catalysts.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cohort_breakout_applied_catalysts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('cohort_breakout_applied_catalysts');
+  }
+};
