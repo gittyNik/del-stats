d497a880e9e169af09330c32b83dd749b3bd677e
vaulstein <vaulstein@gmail.com>
2021-03-24 21:37:18 +0530

    Added auth on ops routes

--- src/routes/learning/ops.routes.js
+++ src/routes/learning/ops.routes.js
@@ -2,8 +2,10 @@ import Express from 'express';
 import breakoutRouter from './ops_breakout.routes';
 import reviewRouter from './ops_review.routes';
 import assessmentRouter from './ops_assessment.routes';
+import authenticate from '../../controllers/auth/auth.controller';
 
 const router = Express.Router();
+router.use(authenticate);
 
 router.use('/breakouts', breakoutRouter);
 router.use('/reviews', reviewRouter);
