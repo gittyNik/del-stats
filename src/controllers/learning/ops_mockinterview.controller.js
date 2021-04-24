9bff0bd2a5a96d2bea667846e30e70c92948b27b
Niskarsh <niskarshk@gmail.com>
2021-04-25 01:54:06 +0530

    Relevant controllers for mock interview slots are added

--- src/controllers/learning/ops_mockinterview.controller.js
+++ src/controllers/learning/ops_mockinterview.controller.js
@@ -1,9 +1,9 @@
 // import Sequelize from 'sequelize';
-import { slotData } from '../../models/mock_interview_slots';
+import { slotData, deleteSlotById, updateSlotById } from '../../models/mock_interview_slots';
 import { createMockInterviewsForMultipleCohort_afterCapstone } from '../../models/mock_interviews';
 import logger from '../../util/logger';
 
-const createMockInterviewsSlotsApi = (req, res) => {
+export const createMockInterviewsSlotsApi = (req, res) => {
   const { cohort_duration, program } = req.body;
   slotData(cohort_duration, program)
     .then(data => res.status(201).send({
@@ -17,7 +17,7 @@ const createMockInterviewsSlotsApi = (req, res) => {
     });
 };
 
-const createMockInterviewsApi = (req, res) => {
+export const createMockInterviewsApi = (req, res) => {
   const {
     cohorts, start_date, learners_exclude, program,
   } = req.body;
@@ -34,7 +34,36 @@ const createMockInterviewsApi = (req, res) => {
       return res.status(500);
     });
 };
-export {
-  createMockInterviewsSlotsApi,
-  createMockInterviewsApi,
+
+export const updateMockInterviewsSlotsByIdApi = (req, res) => {
+  const { id } = req.params;
+  const {
+    cohort_duration, mock_interview_day, time_scheduled, slot_status,
+  } = req.body;
+  updateSlotById({
+    id, cohort_duration, mock_interview_day, time_scheduled, slot_status,
+  })
+    .then(data => res.status(201).send({
+      data,
+      message: 'Mock Interviews slot successfully updated',
+      type: 'success',
+    }))
+    .catch(err => {
+      logger.error(err);
+      return res.status(500);
+    });
+};
+
+export const deleteMockInterviewsSlotsByIdApi = (req, res) => {
+  const { id } = req.params;
+  deleteSlotById({ id })
+    .then(data => res.status(201).send({
+      data,
+      message: 'Mock Interviews slot successfully deleted',
+      type: 'success',
+    }))
+    .catch(err => {
+      logger.error(err);
+      return res.status(500);
+    });
 };
