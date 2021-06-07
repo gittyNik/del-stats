23cec190c5851e16d5e41493dc7b7cfdb81eff79
vaulstein <vaulstein@gmail.com>
2021-06-07 14:03:36 +0530

    Removed unnecessary logs

--- src/integrations/slack/delta-app/utility/chat.js
+++ src/integrations/slack/delta-app/utility/chat.js
@@ -4,9 +4,7 @@ import logger from '../../../../util/logger';
 export const postMessage = async ({
   channel, text, blocks,
 }) => {
-  console.log(`***************`)
   try {
-    console.log(`***************`, web)
     const res = await web.chat.postMessage({
       channel,
       text,
